c9cda63e28959b1f1b4059dd881a91024f7a7953
Naveen <dexter2305@gmail.com>
2020-03-28 22:13:21 +0000

    Fixed scalatest issue with gradle. Removed sbt references

--- src/test/scala/edu.learnings/CubeCalculatorTest.scala
+++ src/test/scala/edu.learnings/CubeCalculatorTest.scala
@@ -2,10 +2,13 @@ package edu.learnings;
 
 //import org.scalatest.Assertions._
 import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 
+@RunWith(classOf[JUnitRunner])
 class CubeCalculatorTest extends FunSuite {
   test("CubeCalculator.cube") {
-    assert(CubeCalculator.cube(3) === 7)
+    assert(CubeCalculator.cube(3) === 27)
   }
 
   test("CubeCalculator.fcube") {
