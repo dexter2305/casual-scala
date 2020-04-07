88779ca7c99cee75f9e2cab2846d4ebe199ba305
Naveen <dexter2305@gmail.com>
2020-04-08 02:20:44 +0530

    Counting Elements #leetcode #30daychallenge

---
+++ src/main/scala/edu/learnings/lc/CountingElements.scala
@@ -0,0 +1,12 @@
+package edu.learnings.lc
+
+object CountingElements {
+  def main(args: Array[String]): Unit = {
+    countingElements(Array(1, 2, 3))
+  }
+
+  def countingElements(arr: Array[Int]): Int = {
+    val givenSet = arr.toSet
+    arr.map(_ + 1).count(e => givenSet.contains(e))
+  }
+}
