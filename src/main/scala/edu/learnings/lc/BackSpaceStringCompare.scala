4e47755665fd0a4c67915e61b4c2aeea92f3ce64
Naveen <dexter2305@gmail.com>
2020-04-13 17:38:34 +0530

    BackSpaceStringCompare #leetcode #30daychallenge

---
+++ src/main/scala/edu/learnings/lc/BackSpaceStringCompare.scala
@@ -0,0 +1,23 @@
+package edu.learnings.lc
+
+object BackSpaceStringCompare {
+
+  def main(args: Array[String]): Unit = {
+    backspaceString("abc#d")
+  }
+
+  def backspaceCompare(S: String, T: String): Boolean = {
+    backspaceString(S) equals backspaceString(T)
+  }
+
+  def backspaceString(input: String): String = {
+    input.foldLeft("")((acc, e) => {
+      val output = e match {
+        case '#' => if (acc.length > 0) acc.substring(0, acc.length - 1) else acc
+        case _ => acc + e
+      }
+      output
+    })
+  }
+}
+
---
+++ src/test/scala/edu.learnings/BackSpaceStringCompareSuite.scala
@@ -0,0 +1,44 @@
+package edu.learnings
+import edu.learnings.lc.BackSpaceStringCompare
+class BackSpaceStringCompareSuite extends BaseSuite {
+
+  test("abc# == ab"){
+    assert(BackSpaceStringCompare.backspaceCompare("abc#", "ab"))
+  }
+
+  test("abc## == a"){
+    assert(BackSpaceStringCompare.backspaceCompare("abc##", "a"))
+  }
+
+  test("## == ''"){
+    assert(BackSpaceStringCompare.backspaceCompare("##", ""))
+  }
+
+  test("abc == abc"){
+    assert(BackSpaceStringCompare.backspaceCompare("abc", "abc"))
+  }
+
+  test("abb#c#c == abc"){
+    assert(BackSpaceStringCompare.backspaceCompare("abb#c#c", "abc"))
+  }
+
+  test(" #a == a"){
+    assert(BackSpaceStringCompare.backspaceCompare(" #a", "a"))
+  }
+
+  test("ab## == c#d#"){
+    assert(BackSpaceStringCompare.backspaceCompare("ab##", "c#d#"))
+  }
+
+  test("ab#c == ad#c"){
+    assert(BackSpaceStringCompare.backspaceCompare("ab#c", "ad#c"))
+  }
+
+  test("a##c == #a#c"){
+    assert(BackSpaceStringCompare.backspaceCompare("a##c", "#a#c"))
+  }
+
+  test("a#c == b"){
+    assert(!BackSpaceStringCompare.backspaceCompare("a#c", "b"))
+  }
+}
