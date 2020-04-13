114070b5c65c7cf843796908b28db603d6a9a368
Naveen <dexter2305@gmail.com>
2020-04-13 17:42:40 +0530

    BackSpaceStringCompare #leetcode #30daychallenge; Updated type for parameters for lambda

--- src/main/scala/edu/learnings/lc/BackSpaceStringCompare.scala
+++ src/main/scala/edu/learnings/lc/BackSpaceStringCompare.scala
@@ -11,7 +11,7 @@ object BackSpaceStringCompare {
   }
 
   def backspaceString(input: String): String = {
-    input.foldLeft("")((acc, e) => {
+    input.foldLeft("")((acc:String, e:Char) => {
       val output = e match {
         case '#' => if (acc.length > 0) acc.substring(0, acc.length - 1) else acc
         case _ => acc + e
