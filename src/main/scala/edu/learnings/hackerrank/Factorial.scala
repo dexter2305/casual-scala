ce731423dce3f1f6c945f72ee1733109cc197b23
Naveen <dexter2305@gmail.com>
2020-08-21 02:32:07 +0530

    Factorial #hackerrank #30daychallenge #day9

---
+++ src/main/scala/edu/learnings/hackerrank/Factorial.scala
@@ -0,0 +1,21 @@
+package edu.learnings.hackerrank
+
+object Factorial {
+
+  def main(args: Array[String]): Unit = {
+    val n = scala.io.StdIn.readInt()
+    println(s"factorial($n):${factorial(n)}")
+  }
+
+  def factorial(n: Int): Int = {
+    @scala.annotation.tailrec
+    def helper(n: Int, acc: Int =1): Int = {
+      n match {
+        case 0 => 1
+        case 1 => acc
+        case _ => helper(n -1, n * acc)
+      }
+    }
+    helper(n)
+  }
+}
