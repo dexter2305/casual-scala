1cce3ae89f92c330a2c51717319ddc6c7526c560
Naveen <dexter2305@gmail.com>
2020-08-20 20:27:06 +0530

    Fixed compilation issue

--- src/main/scala/edu/learnings/ib/PlusOne.scala
+++ src/main/scala/edu/learnings/ib/PlusOne.scala
@@ -1,11 +1,13 @@
 package edu.learnings.ib
 
+import scala.annotation.tailrec
+
 object PlusOne {
 
   def main(args: Array[String]): Unit = {
 
-    println(s"res: ${plusOne(Array(1, 2, 3)) mkString (" ")}")
-    println(s"res: ${plusOne(Array(9, 9)) mkString (" ")}")
+    println(s"res: ${plusOne(Array(1, 2, 3)) mkString " "}")
+    println(s"res: ${plusOne(Array(9, 9)) mkString " "}")
 
   }
 
@@ -14,7 +16,7 @@ object PlusOne {
 
     @scala.annotation.tailrec
     def addOne(numbers: List[Int], acc: List[Int], carry: Int): List[Int] = {
-      println(s"processing input: ${numbers mkString (" ")} with carry:$carry & acc:${acc.mkString(" ")}")
+      println(s"processing input: ${numbers mkString " "} with carry:$carry & acc:${acc.mkString(" ")}")
       if (carry == 0) numbers.reverse ::: acc
       else numbers match {
         case head :: tail => addOne(tail, (head + carry) % 10 +: acc, (head + carry) / 10)
@@ -22,10 +24,11 @@ object PlusOne {
       }
     }
 
+    @tailrec
     def dropLeadingZero(ns: List[Int]): List[Int] = {
       ns match {
-        case 0 :: tail => dropLeadingZero(tail)
         case 0 :: Nil => List(0)
+        case 0 :: tail => dropLeadingZero(tail)
         case _ => ns
       }
     }
