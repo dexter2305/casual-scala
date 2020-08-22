d142933da299bdf24db9ec63fc2a60c8f902d21e
Naveen <dexter2305@gmail.com>
2020-08-22 10:23:31 +0530

    RegEx #hackerrank #30daychallenge #day25 #o(sqrt(n))

---
+++ src/test/scala/edu/learnings/hackerrank/PrimeSuite.scala
@@ -0,0 +1,26 @@
+package edu.learnings.hackerrank
+
+import edu.learnings.BaseSuite
+
+class PrimeSuite extends BaseSuite {
+
+  test("0 is not a prime number") {
+    assertResult(false)(Prime.isPrime(0))
+  }
+  test("1 is not a prime number") {
+    assertResult(false)(Prime.isPrime(1))
+  }
+  test("2 is a prime number") {
+    assertResult(true)(Prime.isPrime(2))
+  }
+
+  test("11 is a prime number") {
+    assertResult(true)(Prime.isPrime(11))
+  }
+
+  test("20 is not a prime number") {
+    assertResult(false)(Prime.isPrime(20))
+  }
+
+
+}
