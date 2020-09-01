40d96ce850bc12834369732ff40d1f6216a324a9
Naveen <dexter2305@gmail.com>
2020-09-01 23:53:45 +0530

    Minimax #hackerrank

---
+++ src/main/scala/edu/learnings/hackerrank/MiniMaxSum.scala
@@ -0,0 +1,18 @@
+package edu.learnings.hackerrank
+
+object MiniMaxSum {
+
+  def main(args: Array[String]): Unit = {
+
+    miniMaxSum(Array(1, 2, 3, 4, 5))
+  }
+
+  def miniMaxSum(arr: Array[Int]): Unit = {
+    println(getVal(arr))
+  }
+
+  def getVal(arr: Array[Int]): String = {
+    val sorted = arr.map(_.toLong).sorted
+    s"${sorted.take(4).sum} ${sorted.reverse.take(4).sum}"
+  }
+}
