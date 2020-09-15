335f7aef477d879048f27f409aa825cfe9be36e5
Naveen <dexter2305@gmail.com>
2020-09-15 09:16:26 +0530

    CountVowels #codewars #PatternMatching

---
+++ src/main/scala/edu/learnings/codewars/CountVowels.scala
@@ -0,0 +1,11 @@
+package edu.learnings.codewars
+
+class CountVowels {
+
+  def getCount(s: String): Int = {
+    s.toCharArray map(_.toString) count {
+      case "a" | "e" | "i" | "o" | "u" => true
+      case _ => false
+    }
+  }
+}
