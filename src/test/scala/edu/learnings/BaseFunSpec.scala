99efe2a6445cbdc7bb7d47b654a466ae9323b442
Naveen <dexter2305@gmail.com>
2020-09-15 21:44:56 +0530

    Added extension of FunSpec

---
+++ src/test/scala/edu/learnings/BaseFunSpec.scala
@@ -0,0 +1,8 @@
+package edu.learnings
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSpec
+import org.scalatestplus.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class BaseFunSpec extends FunSpec
