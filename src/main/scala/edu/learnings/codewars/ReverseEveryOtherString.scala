7df81f817737472283d284711dc01a2e55070892
Naveen <dexter2305@gmail.com>
2020-11-27 01:58:13 +0530

    GradingStudents #hackerrank ReverseEveryOtherString #codewars

---
+++ src/main/scala/edu/learnings/codewars/ReverseEveryOtherString.scala
@@ -0,0 +1,9 @@
+package edu.learnings.codewars
+
+class ReverseEveryOtherString {
+  def reverse(str: String): String = {
+    val strings = str.split(" ").map(_.trim)
+    val rsArray = for (i <- 0 to strings.length) yield if (i % 2 == 0) strings(i) else strings(i).reverse
+    rsArray.mkString(" ")
+  }
+}
