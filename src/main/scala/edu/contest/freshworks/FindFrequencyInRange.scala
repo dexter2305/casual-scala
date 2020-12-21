49c75118fe195298f4f29eccf361fb7a6b93c106
Naveen <dexter2305@gmail.com>
2020-12-22 01:13:21 +0530

    Find frequence of char in given string #freshworks

---
+++ src/main/scala/edu/contest/freshworks/FindFrequencyInRange.scala
@@ -0,0 +1,21 @@
+package edu.contest.freshworks
+
+class FindFrequencyInRange {
+
+  def frequencyInRange(input: String, q: Char, limit: Int, patternLength: Int): Int = {
+
+    val pattern = input.slice(0, patternLength)
+    val cCountMap: Map[Char, Int] = pattern.toCharArray.groupBy(identity).map(t => (t._1, t._2.length))
+    val x: Int = cCountMap.getOrElse(q, 0) * (limit / patternLength)
+    val y: Int = pattern.slice(0, limit % patternLength)
+      .toCharArray
+      .groupBy(id => id)
+      .map(t => (t._1, t._2.length))
+      .getOrElse(q, 0)
+
+    println(s"pattern: $pattern, dictionary: $cCountMap, x: $x, y: $y")
+
+    x + y
+  }
+
+}
