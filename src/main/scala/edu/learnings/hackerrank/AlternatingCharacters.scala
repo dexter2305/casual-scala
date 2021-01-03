46c65672cfb6b9694f2b0bd39c4c3514fa31601f
Naveen <dexter2305@gmail.com>
2021-01-04 01:37:02 +0530

    AlternatingCharacters #hackerrank

---
+++ src/main/scala/edu/learnings/hackerrank/AlternatingCharacters.scala
@@ -0,0 +1,23 @@
+package edu.learnings.hackerrank
+
+class AlternatingCharacters {
+
+  def alternatingCharacters(s: String): Int = {
+
+    val (_, deleteCounter) = s.toCharArray.foldLeft((' ', 0))((t, e) => {
+     val x:(Char, Int) = if (e equals t._1) (t._1, t._2 + 1)
+      else (e, t._2)
+      //println(s"char: $e & acc: $x")
+      x
+    })
+    deleteCounter
+  }
+}
+
+object AlternatingCharacters {
+  def main(args: Array[String]): Unit = {
+    val i = new AlternatingCharacters
+    val r = i.alternatingCharacters("AAB")
+    println(s"$r")
+  }
+}
