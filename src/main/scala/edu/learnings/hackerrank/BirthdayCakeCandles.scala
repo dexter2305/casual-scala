7ee4e340e44c3eec6447a1c453ad9be73442fdc5
Naveen <dexter2305@gmail.com>
2021-01-04 13:21:14 +0530

    Warm up challenges #hackerrank

---
+++ src/main/scala/edu/learnings/hackerrank/BirthdayCakeCandles.scala
@@ -0,0 +1,17 @@
+package edu.learnings.hackerrank
+
+class BirthdayCakeCandles {
+
+  def birthdayCakeCandles(candles: Array[Int]): Int = {
+    val g = candles.groupBy(identity)
+    g(g.keys.max).length
+
+  }
+}
+
+object BirthdayCakeCandles {
+  def main(args: Array[String]) : Unit = {
+    val i = new BirthdayCakeCandles
+    i.birthdayCakeCandles(Array(4,3,2,1))
+  }
+}
