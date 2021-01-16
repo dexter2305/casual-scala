e54847e6e5bcb4bbe57913c1bb16e613edaf8d09
Naveen <dexter2305@gmail.com>
2021-01-17 02:07:22 +0530

    CaesarCipher #hackerrank

---
+++ src/main/scala/edu/learnings/hackerrank/CaesarCipher.scala
@@ -0,0 +1,23 @@
+package edu.learnings.hackerrank
+
+
+class CaesarCipher {
+
+
+  def caesarCipher(s: String, k: Int): String = {
+
+    val transform: (Char, Int) => Char = (x, k) => {
+      if (x.isLetter) {
+        val rotate = k % 26
+        val pxFormed = x.toInt + rotate
+        val asNumber = if (x.isUpper && pxFormed > 'Z'.toInt) pxFormed - 26
+        else if (x.isLower && pxFormed > 'z'.toInt) pxFormed - 26
+        else pxFormed
+        //println(s"$x with rotate: $rotate => $asNumber")
+        asNumber.toChar
+      } else x
+    }
+    val res = s.map(transform(_, k))
+    res
+  }
+}
---
+++ src/test/scala/edu/learnings/hackerrank/CaesarCipherSpec.scala
@@ -0,0 +1,27 @@
+package edu.learnings.hackerrank
+
+import edu.learnings.BaseFlatSpec
+
+class CaesarCipherSpec extends BaseFlatSpec {
+
+  val i = new CaesarCipher
+  "a with rotate 1" should "be b" in {
+    i.caesarCipher("a", 1) shouldBe "b"
+  }
+
+  "z with rotate 1" should "be a" in {
+    i.caesarCipher("z", 1) shouldBe "a"
+  }
+
+  "A with rotate 1" should "be B" in {
+    i.caesarCipher("A", 1) shouldBe "B"
+  }
+
+  "Z with rotate 1" should "be A" in {
+    i.caesarCipher("A", 1) shouldBe "B"
+  }
+
+  "z with rotate 27" should "be a" in {
+    i.caesarCipher("z", 27) shouldBe "a"
+  }
+}
