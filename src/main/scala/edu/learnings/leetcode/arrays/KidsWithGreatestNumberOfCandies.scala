7c10da4114e467183dbab2df35e6444d79d038e9
Naveen <dexter2305@gmail.com>
2021-01-24 01:02:47 +0530

    refactored

--- src/main/scala/edu/learnings/leetcode/arrays/KidsWithGreatestNumberOfCandies.scala
+++ src/main/scala/edu/learnings/leetcode/arrays/KidsWithGreatestNumberOfCandies.scala
@@ -1,4 +1,4 @@
-package edu.learnings.leetcode
+package edu.learnings.leetcode.arrays
 
 class KidsWithGreatestNumberOfCandies {
   def kidsWithCandies(candies: Array[Int], extraCandies: Int): Array[Boolean] = {
--- src/test/scala/edu/learnings/leetcode/arrays/KidsWithGreatestNumberOfCandiesSpec.scala
+++ src/test/scala/edu/learnings/leetcode/arrays/KidsWithGreatestNumberOfCandiesSpec.scala
@@ -1,4 +1,4 @@
-package edu.learnings.leetcode
+package edu.learnings.leetcode.arrays
 
 import edu.learnings.BaseFlatSpec
 
