910e88bf7c7d77e13c8a2943545b3cfa00be373b
Naveen <dexter2305@gmail.com>
2021-04-11 01:41:44 +0530

    Remove elements #leetcode #arrays

---
+++ src/main/scala/edu/learnings/leetcode/arrays/RemoveElement.scala
@@ -0,0 +1,30 @@
+package edu.learnings.leetcode.arrays
+
+object RemoveElement extends App {
+
+  val l = Array(3, 2, 2, 3)
+  val length = removeElement(l, 3)
+  println(s"l = $length && ${l.take(length).mkString("-")}")
+
+  def removeElement(nums: Array[Int], `val`: Int): Int = {
+
+    @scala.annotation.tailrec
+    def go(a: Array[Int], s: Int, tracker: Int, parser: Int): Int = {
+      if (tracker < a.length && parser < a.length) {
+        if (a(tracker) != s) go(a, s, tracker + 1, parser + 1)
+        else {
+          if (a(parser) == s) go(a, s, tracker, parser + 1)
+          else {
+            a(tracker) = a(parser)
+            a(parser) = s
+            go(a, s, tracker + 1, tracker + 1)
+          }
+        }
+      } else {
+        tracker
+      }
+    }
+
+    if (nums.isEmpty) 0 else go(nums, `val`, 0, 0)
+  }
+}
