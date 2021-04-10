b65cac109c104504710aa887d04e082a4d7fa78e
Naveen <dexter2305@gmail.com>
2021-04-10 14:12:54 +0530

    - Check if N and its double exits #leetcode #arrays - Random Generator using Lazylist.iterate

--- src/main/scala/com/packt/falgo/Launcher.scala
+++ src/main/scala/com/packt/falgo/Launcher.scala
@@ -2,7 +2,7 @@ package com.packt.falgo
 
 object Launcher extends App {
 
-  val randomGenerator = new RandomGenerator
+  val randomGenerator = new RandomGenerator(90)
   println(s"${randomGenerator.generateFormatted(10)}")
 
 }
---
+++ src/main/scala/edu/learnings/leetcode/arrays/CheckIfNAndItsDoubleExists.scala
@@ -0,0 +1,32 @@
+package edu.learnings.leetcode.arrays
+
+object CheckIfNAndItsDoubleExists extends App {
+
+  println(s"${CheckIfNAndItsDoubleExists.checkIfExist(Array(3,1,7,11))}")
+
+
+  def checkIfExist(arr: Array[Int]): Boolean = {
+    @scala.annotation.tailrec
+    def go(nums: Array[Int], index: Int, pc: Set[Double]) : Boolean = {
+      if (index < nums.length) {
+        if (pc.isEmpty) go (nums, index + 1, pc + nums(index).toDouble)
+        else {
+          val v:Double = nums(index)
+          if (pc.contains(v * 2)) {
+            println(s"${pc.mkString(",")} has double of ${nums(index)}")
+            true
+          }
+          else if (pc.contains(v / 2 ) ) {
+            println(s"${pc.mkString(",")} has half of ${nums(index)}")
+            true
+          }
+          else go(nums, index + 1, pc + nums(index).toDouble)
+        }
+      } else {
+        println("reached eol. return false")
+        false
+      }
+    }
+    go(arr, 0, Set.empty[Double])
+  }
+}
