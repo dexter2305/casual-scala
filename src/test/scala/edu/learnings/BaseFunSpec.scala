1fec3edddfed182d876f65635895bf3c9a1759d8
Naveen <dexter2305@gmail.com>
2021-04-10 19:28:37 +0530

    Remove Duplicates from Sorted Array #leetcode #arrays

--- src/test/scala/edu/learnings/BaseFunSpec.scala
+++ src/test/scala/edu/learnings/BaseFunSpec.scala
@@ -1,5 +1,10 @@
 package edu.learnings
 
+import org.scalatest.concurrent.TimeLimitedTests
 import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.time.{Seconds, Span}
 
-class BaseFunSpec extends AnyFunSpec
+class BaseFunSpec extends AnyFunSpec with Matchers with TimeLimitedTests {
+  override def timeLimit: Span = Span(7, Seconds)
+}
