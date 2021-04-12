5954176b55745519b0d6632c8ca72fc263d53c7e
Naveen <dexter2305@gmail.com>
2021-04-13 02:49:29 +0530

    MyLinkedList #leetcode #linkedlist

--- src/test/scala/edu/learnings/BaseFunSpec.scala
+++ src/test/scala/edu/learnings/BaseFunSpec.scala
@@ -1,10 +1,11 @@
 package edu.learnings
 
+import org.scalatest.GivenWhenThen
 import org.scalatest.concurrent.TimeLimitedTests
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatest.time.{Seconds, Span}
 
-class BaseFunSpec extends AnyFunSpec with Matchers with TimeLimitedTests {
+class BaseFunSpec extends AnyFunSpec with Matchers with TimeLimitedTests with GivenWhenThen {
   override def timeLimit: Span = Span(7, Seconds)
 }
