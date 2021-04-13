206bbe9b87ff0b00866c09d9104fc8d9b630c551
Naveen <dexter2305@gmail.com>
2021-04-13 19:00:55 +0530

    LinkedList to detect cycles or loops #leetcode #linkedlist

--- src/main/scala/edu/learnings/leetcode/linkedlists/LinkedListCycle.scala
+++ src/main/scala/edu/learnings/leetcode/linkedlists/LinkedListCycle.scala
@@ -7,13 +7,39 @@ class ListNode(var _x: Int = 0) {
 }
 
 object LinkedListCycle extends App {
+
+
   def hasCycle(head: ListNode): Boolean = {
     @scala.annotation.tailrec
-    def go(slow: ListNode, fast: ListNode) : Boolean = {
+    def go(slow: ListNode, fast: ListNode): Boolean = {
       if (fast == null || fast.next == null) false
       else if (fast == slow) true
       else go(slow.next, fast.next.next)
     }
-    if (head == null) false else go(head, head.next)
+
+    if (head == null || head.next == null) false else go(head.next, head.next.next)
+  }
+
+  def detectCycle(head: ListNode): ListNode = {
+    @scala.annotation.tailrec
+    def hasMeetingNode(slow: ListNode, fast: ListNode): Option[ListNode] = {
+      if (fast == null || fast.next == null) None
+      else if (fast == slow) Some(slow)
+      else hasMeetingNode(slow.next, fast.next.next)
+    }
+
+    @scala.annotation.tailrec
+    def getLoopStartNode(x: ListNode, y: ListNode): ListNode = {
+      println(s"(${x.x}, ${y.x})")
+      if (x == y) x else getLoopStartNode(x.next, y.next)
+    }
+
+    if (head == null || head.next == null) null else {
+      hasMeetingNode(head.next, head.next.next) match {
+        case None => null
+        case Some(meetingNode) => println(s"meeting node:${meetingNode.x}")
+          getLoopStartNode(head, meetingNode)
+      }
+    }
   }
 }
--- src/test/scala/edu/learnings/leetcode/linkedlists/LinkedListCycleSpec.scala
+++ src/test/scala/edu/learnings/leetcode/linkedlists/LinkedListCycleSpec.scala
@@ -1,50 +1,84 @@
 package edu.learnings.leetcode.linkedlists
 
-import edu.learnings.BaseFlatSpec
+import edu.learnings.{BaseFlatSpec, BaseFunSpec}
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
-class LinkedListCycleSpec extends BaseFlatSpec {
+class LinkedListCycleSpec extends BaseFunSpec {
 
-  "An empty list" should "not have cycle" in {
-    LinkedListCycle.hasCycle(null) shouldBe false
+  describe("LinkedList") {
+    describe("hasCycle") {
+      it("should be false for empty list") {
+        LinkedListCycle.hasCycle(null) shouldBe false
+      }
+      it("should be false only head not referring to itself as next") {
+        LinkedListCycle.hasCycle(new ListNode()) shouldBe false
+      }
+      it("should return false for 1->2->3") {
+        val one = new ListNode(1)
+        val two = new ListNode(2)
+        val three = new ListNode(3)
+        one.next = two
+        two.next = three
+        LinkedListCycle.hasCycle(one) shouldBe false
+      }
+      it("should be true for 1-2-3-4-2") {
+        val one = new ListNode(1)
+        val two = new ListNode(2)
+        val three = new ListNode(3)
+        val four = new ListNode(4)
+        one.next = two
+        two.next = three
+        three.next = four
+        four.next = two
+        LinkedListCycle.hasCycle(one) shouldBe true
+      }
+      it("should find cycle in single referring to itself as next") {
+        val node = new ListNode(3)
+        node.next = node
+        LinkedListCycle.hasCycle(node) shouldBe true
+      }
+      it("should find cycle when last node points to self") {
+        val one = new ListNode(1)
+        val two = new ListNode(2)
+        val three = new ListNode(3)
+        val four = new ListNode(4)
+        one.next = two
+        two.next = three
+        three.next = four
+        four.next = four
+        LinkedListCycle.hasCycle(one) shouldBe true
+      }
+    }
+    describe("detectCycle"){
+      it ("should detect start of cycles in linkedlist"){
+        Given("list of 1-2-3-4-5-6-2")
+        val one = new ListNode(1)
+        val two = new ListNode(2)
+        val three = new ListNode(3)
+        val four = new ListNode(4)
+        val five = new ListNode(5)
+        val six = new ListNode(6)
+        one.next = two
+        two.next = three
+        three.next = four
+        four.next = five
+        five.next = six
+        six.next = two
+        Then("cycle should be detected")
+        LinkedListCycle.hasCycle(one) shouldBe true
+        And("2 must be returned as start of cycle")
+        LinkedListCycle.detectCycle(one) shouldBe two
+      }
+      it ("should return null when no cycle is found"){
+        Given("list with no cycle")
+        val one = new ListNode(1)
+        val two = new ListNode(2)
+        one.next = two
+        Then("returns null")
+        LinkedListCycle.detectCycle(one) shouldBe null
+      }
+    }
   }
 
-  "List with only head " should "not have cycle" in {
-    val node = new ListNode()
-    LinkedListCycle.hasCycle(node) shouldBe false
-  }
-
-  "1->2->3" should "not have cycle" in {
-    val one = new ListNode(1)
-    val two = new ListNode(2)
-    val three = new ListNode(3)
-    one.next = two
-    two.next = three
-
-    LinkedListCycle.hasCycle(one) shouldBe false
-  }
-
-  "1->2->3->4->2" should "have cycle" in  {
-    val one = new ListNode(1)
-    val two = new ListNode(2)
-    val three = new ListNode(3)
-    val four = new ListNode(4)
-    one.next = two
-    two.next = three
-    three.next = four
-    four.next = two
-    LinkedListCycle.hasCycle(one) shouldBe true
-  }
-
-  "last node pointing to itself" should "have cycle" in  {
-    val one = new ListNode(1)
-    val two = new ListNode(2)
-    val three = new ListNode(3)
-    val four = new ListNode(4)
-    one.next = two
-    two.next = three
-    three.next = four
-    four.next = four
-    LinkedListCycle.hasCycle(one) shouldBe true
-  }
 
 }
