ff867260483e6ae89ac70c159059f9eddc85580c
Naveen <dexter2305@gmail.com>
2021-04-15 01:12:57 +0530

    Count of smaller numbers in array #leetcode

---
+++ src/main/scala/edu/learnings/leetcode/linkedlists/NumberOfSmallerNumbersThanCurrent.scala
@@ -0,0 +1,16 @@
+package edu.learnings.leetcode.linkedlists
+
+object NumberOfSmallerNumbersThanCurrent {
+
+  def smallerNumbersThanCurrent(numbers: Array[Int]): Array[Int] = {
+    val frequency = Array.fill[Int](numbers.max + 1)(0)
+    for (n <- numbers) {
+      frequency(n) = frequency(n) + 1
+    }
+    val reference = frequency.scanLeft(0)((a, e) => a + e).tail
+    val result = for (n <- numbers) yield {
+      if (n == 0) 0 else reference(n - 1)
+    }
+    result
+  }
+}
