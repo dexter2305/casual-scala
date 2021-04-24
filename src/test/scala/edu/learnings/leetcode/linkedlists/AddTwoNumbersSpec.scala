264e42d85840b2ba3c87ccc7210751bd840ddedd
Naveen <dexter2305@gmail.com>
2021-04-24 23:22:34 +0530

    Add two numbers #linkedlist #leetcode

---
+++ src/test/scala/edu/learnings/leetcode/linkedlists/AddTwoNumbersSpec.scala
@@ -0,0 +1,22 @@
+package edu.learnings.leetcode.linkedlists
+
+import edu.learnings.BaseFlatSpec
+
+class AddTwoNumbersSpec extends BaseFlatSpec {
+
+  "AddTwoNumbers" should "return [7,0,8] from [2,4,3][5,6,4]" in {
+    val a = AddTwoNumbers.addTwoNumbers(ListNode(Array(2, 4, 3)), ListNode(Array(5, 6, 4)))
+    assert(ListNode.sameElements(ListNode(Array(7, 0, 8)), a), s"${ListNode.asString(a)}")
+  }
+
+  ignore should "return [2,4,6,4] from [4,3,2,1][3,2,1]" in {
+    val a = AddTwoNumbers.addTwoNumbers(ListNode(Array(4, 3, 2, 1)), ListNode(Array(3, 2, 1)))
+    assert(ListNode.sameElements(ListNode(Array(2, 4, 6, 4)), a), s"${ListNode.asString(a)}")
+  }
+
+  it should "return [7,0,4,0,1]  for [2,4,9][5,6,4,9]" in {
+    val a = AddTwoNumbers.addTwoNumbers(ListNode(Array(2,4,9)), ListNode(Array(5,6,4,9)))
+    assert(ListNode.sameElements(ListNode(Array(7,0,4,0,1)), a), s"${ListNode.asString(a)}")
+  }
+
+}
