ddcaf85c1ab4e5d3f1cc356671c3289cc8f6cb33
Naveen <dexter2305@gmail.com>
2021-05-04 02:45:40 +0530

    Dominant Index case #leetcode #arrays.

--- src/main/scala/edu/learnings/leetcode/arrays/ArrayProblems.scala
+++ src/main/scala/edu/learnings/leetcode/arrays/ArrayProblems.scala
@@ -27,4 +27,26 @@ object ArrayProblems {
 
     go(nums, 0, 0, nums.sum)
   }
+
+  //https://leetcode.com/explore/learn/card/array-and-string/201/introduction-to-array/1147/
+  def dominantIndex(nums: Array[Int]): Int = {
+    @scala.annotation.tailrec
+    def maxWithPosition(n: Array[Int], i: Int, max: Int, maxIndex: Int): (Int, Int) = {
+      i match {
+        case currentIndex if currentIndex < n.length => if (n(currentIndex) > max) maxWithPosition(n, currentIndex + 1, n(currentIndex), currentIndex)
+        else maxWithPosition(n, currentIndex + 1, max, maxIndex)
+        case _ => (max, maxIndex)
+      }
+    }
+
+    nums.length match {
+      case 0 => -1
+      case 1 => 0
+      case _ => val (max, position) = maxWithPosition(nums, 1, nums(0), 0)
+        nums.filter(_ != max).find(2 * _ > max) match {
+          case Some(_) => -1
+          case None => position
+        }
+    }
+  }
 }
--- src/test/scala/edu/learnings/leetcode/arrays/ArrayProblemsSpec.scala
+++ src/test/scala/edu/learnings/leetcode/arrays/ArrayProblemsSpec.scala
@@ -1,7 +1,6 @@
 package edu.learnings.leetcode.arrays
 
 import edu.learnings.BaseFunSpec
-import edu.learnings.leetcode.arrays
 import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
 class ArrayProblemsSpec extends BaseFunSpec {
@@ -15,16 +14,24 @@ class ArrayProblemsSpec extends BaseFunSpec {
 
     describe("pivotIndex") {
       it("should return -1 when no pivot exists") {
-        ArrayProblems.pivotIndex(Array(1,2,3)) shouldBe -1
+        ArrayProblems.pivotIndex(Array(1, 2, 3)) shouldBe -1
       }
 
-      it ("should return pivot element when lsum == rsum"){
-        ArrayProblems.pivotIndex(Array(2,1,-1)) shouldBe 0
+      it("should return pivot element when lsum == rsum") {
+        ArrayProblems.pivotIndex(Array(2, 1, -1)) shouldBe 0
       }
 
-      it ("should return 0 when length of array == 1") (ArrayProblems.pivotIndex(Array(1)) shouldBe 0)
+      it("should return 0 when length of array == 1")(ArrayProblems.pivotIndex(Array(1)) shouldBe 0)
 
-      it ("should return 0 for [0,0,0]") (ArrayProblems.pivotIndex(Array(0,0,0)) shouldBe 0)
+      it("should return 0 for [0,0,0]")(ArrayProblems.pivotIndex(Array(0, 0, 0)) shouldBe 0)
+    }
+
+    describe("dominantIndex") {
+      it("should return -1 when array length == 1")(ArrayProblems.dominantIndex(Array(1)) shouldBe 0)
+      it("should return -1 when no dominant index exists") (ArrayProblems.dominantIndex(Array(1,2,3,4)) shouldBe -1)
+      it("should return position of dominant index when only one exists")(ArrayProblems.dominantIndex(Array(1,2,3,8)) shouldBe 3)
+      it("should return position of first dominant index when multiple exist")(ArrayProblems.dominantIndex(Array(1,8,2,3,8)) shouldBe 1)
+      it("should return position of dominant index even second max element is 2 * maxval")(ArrayProblems.dominantIndex(Array(4,8)) shouldBe 1)
     }
   }
 }
