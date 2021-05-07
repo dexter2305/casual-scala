8cd57b9a34ccf2cf42503df0c64a2f07f58d89ae
Naveen <dexter2305@gmail.com>
2021-05-08 03:10:56 +0530

    Added TreeNode class #leetcode #binarytree

---
+++ src/main/scala/edu/learnings/leetcode/binarytree/TreeNode.scala
@@ -0,0 +1,7 @@
+package edu.learnings.leetcode.binarytree
+
+case class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+  var value: Int = _value
+  var left: TreeNode = _left
+  var right: TreeNode = _right
+}
